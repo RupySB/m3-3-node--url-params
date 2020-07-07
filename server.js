1260b4ce5d7d0b0385e27cda1c78e5f56c09fc57
Rupinder Bagri <RBagri94@hotmail.com>
2020-07-07 15:28:27 -0400

    server js

--- server.js
+++ server.js
@@ -1,27 +1,51 @@
-'use strict';
+"use strict";
 
-const morgan = require('morgan');
-
-const { top50 } = require('./data/top50');
+const morgan = require("morgan");
+const express = require("express");
+const { top50 } = require("./data/top50");
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+//this is used to make the stuff on line 20 easier to write
 
-app.use(morgan('dev'));
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
-app.set('view engine', 'ejs');
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
 // endpoints here
-
-// handle 404s
-app.git('*', (req, res) => {
-    res.status(404);
-    res.render('pages/fourOhFour', {
-        title: 'I got nothing',
-        path: req.originalUrl
-    });
+app.get("/top50", (req, res) => {
+  res.render("pages/top50", {
+    title: "Top 50 Songs Streamed on Spotify",
+    myName: "Rupy",
+    top50,
+  });
 });
 
-get.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+// handle 404s
+app.get("*", (req, res) => {
+  res.status(404);
+  res.render("pages/fourOhFour", {
+    title: "I got nothing",
+    path: req.originalUrl,
+  });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+//1.5 - popularArtist - I tried it below...not sure how to continue
+// const popularArtist = (req, res) => {
+//     const song.artist = {};
+//     top50.forEach((song) => {
+//         if (song.artist === "Justin Beiber"){
+//             popularArtist.push(song.artist);
+//         }
+//         else (
+//         res.render("pages/fourOhFour", {
+//           title: "I got nothing",
+//           path: req.originalUrl,)
+//         });
+
+//         )
+//     })
